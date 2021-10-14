e518791aecb4489a8c82ea3e0500244cf855887a
Иван Александрович Стахов <50211739+LeftTry@users.noreply.github.com>
2021-10-14 19:21:40 +0300

    Added deletefromblacklist

--- internal/storage/workWithDB.go
+++ internal/storage/workWithDB.go
@@ -145,3 +145,61 @@ func GetAllBadWordsByChat(idd int64) ([]string, error) {
 	return allWords, nil
 
 }
+
+func DeleteWordFromBlacklist(idd int64, badWord string) error {
+	conn, err := db.Connect(context.Background(), "postgres://postgres:password@localhost:5432/test")
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer conn.Close(context.Background())
+
+	var allBadWords []string = strings.Split(badWord, " ")
+
+	var badWordByChat []string = nil
+
+	newRows, Err := conn.Query(context.Background(), "select badword from added_to_chats where id = $1", idd)
+
+	if Err != nil {
+		return Err
+	}
+
+	if newRows == nil {
+		return nil
+	}
+
+	for newRows.Next() {
+		var txt string
+		errWithParse := newRows.Scan(&txt)
+		if errWithParse != nil {
+			return errWithParse
+		}
+		badWordByChat = append(badWordByChat, txt)
+	}
+
+	var flag = false
+	var i = 0
+	var j = 0
+	for i = 0; i < len(allBadWords); i++ {
+		fmt.Println(allBadWords[i])
+		flag = false
+		for j = 0; j < len(badWordByChat); j++ {
+			if allBadWords[i] != badWordByChat[j] {
+				flag = true
+			}
+		}
+
+		if !flag {
+			_, err = conn.Exec(context.Background(), "delete from added_to_chats where (id, badword) values ($1, $2)", idd, allBadWords[i])
+
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+		}
+	}
+
+	return nil
+}
